36fb9e1ea0f0d36029417a8588845538411b92fe
Yeonsu1027 <rito1205@naver.com>
2024-05-31 15:36:22 +0900

    갤러리

---
+++ SpMVC-gallery-05/document/database(2024-05-31).sql
@@ -0,0 +1,14 @@
+-- galleryDB3
+CREATE DATABASE galleryDB3;
+USE galleryDB3;
+SHOW TABLES;
+DESC tbl_gallerys;
+DESC tbl_images;
+
+DROP TABLE tbl_images;
+DROP TABLE tbl_gallerys;
+
+
+SELECT * FROM tbl_images;
+
+SELECT * FROM tbl_gallerys;
